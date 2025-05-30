eb314a30aa8c14303f284bae26b09896813a32e6
k0 <k0src@null.net>
2025-05-30 12:03:44 -0500

    tags

--- public/assets/browse/songs.js
+++ public/assets/browse/songs.js
@@ -3,10 +3,27 @@ const sortDropdownItems = document.querySelectorAll(
   ".result-sort-content--item"
 );
 const filterInput = document.querySelector(".result-filters--input");
+const genreTags = document.querySelectorAll(".result-entry--genre");
+
+/* SORTING */
 
 const sortSongs = function (key) {
   window.location.href = `/browse/songs?sort=${encodeURIComponent(key)}`;
 };
+
+sortDropdownBtn.addEventListener("click", () => {
+  document
+    .querySelector(".result-sort-content")
+    .classList.toggle("result-sort-content--show");
+});
+
+sortDropdownItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    sortSongs(item.id);
+  });
+});
+
+/* FILTERING */
 
 const filterResults = function (query) {
   const lowerQuery = query.trim().toLowerCase();
@@ -32,18 +49,17 @@ const filterResults = function (query) {
   });
 };
 
-sortDropdownBtn.addEventListener("click", () => {
-  document
-    .querySelector(".result-sort-content")
-    .classList.toggle("result-sort-content--show");
-});
-
-sortDropdownItems.forEach((item) => {
-  item.addEventListener("click", () => {
-    sortSongs(item.id);
-  });
-});
-
 filterInput.addEventListener("input", () => {
   filterResults(filterInput.value);
+});
+
+/* APPLYING DYNAMIC GENRE TAG CLASS */
+
+genreTags.forEach((tag) => {
+  tag.classList.add(
+    `result-entry--genre--${tag.textContent
+      .trim()
+      .toLowerCase()
+      .replaceAll(" ", "-")}`
+  );
 });
