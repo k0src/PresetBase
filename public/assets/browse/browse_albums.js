40147c0a5f5ca26a2529e64237add042f0def845
k0 <k0src@null.net>
2025-06-01 01:20:25 -0500

    browse all albumss

---
+++ public/assets/browse/browse_albums.js
@@ -0,0 +1,143 @@
+const sortDropdownBtn = document.querySelector(".result-sort-btn");
+const sortDropdownItems = document.querySelectorAll(
+  ".result-sort-content--item"
+);
+const sortDropdownContent = document.querySelector(".result-sort-content");
+const filterInput = document.querySelector(".result-filter--input");
+const genreTags = document.querySelectorAll(".genre-tag");
+const filterClearBtn = document.querySelector(".result-filter--clear");
+const viewModeBtns = document.querySelectorAll(".results-view-btn");
+
+/* SORTING */
+const sortAlbums = function (key) {
+  window.location.href = `/browse/albums?sort=${encodeURIComponent(key)}`;
+};
+
+sortDropdownBtn.addEventListener("click", () => {
+  sortDropdownContent.classList.toggle("result-sort-content--show");
+});
+
+sortDropdownItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    sortAlbums(item.id);
+  });
+});
+
+/* FILTERING */
+const filterList = function (query) {
+  const lowerQuery = query.trim().toLowerCase();
+
+  document.querySelectorAll(".result-entry").forEach((entry) => {
+    const artist = entry
+      .querySelector(".result-entry--artist-name")
+      .textContent.toLowerCase();
+    const album = entry
+      .querySelector(".result-entry--album-title")
+      .textContent.toLowerCase();
+
+    const matches =
+      !lowerQuery || artist.includes(lowerQuery) || album.includes(lowerQuery);
+
+    entry.classList.toggle("hide", !matches);
+  });
+};
+
+const filterCards = function (query) {
+  const lowerQuery = query.trim().toLowerCase();
+
+  document.querySelectorAll(".card-entry").forEach((entry) => {
+    const artist = entry
+      .querySelector(".card-entry--artist-name")
+      .textContent.toLowerCase();
+    const album = entry
+      .querySelector(".card-entry--album-title")
+      .textContent.toLowerCase();
+
+    const matches =
+      !lowerQuery || artist.includes(lowerQuery) || album.includes(lowerQuery);
+
+    entry.style.display = matches ? "flex" : "none";
+  });
+};
+
+const filterResults = function (query) {
+  filterList(query);
+  filterCards(query);
+};
+
+filterInput.addEventListener("input", () => {
+  filterResults(filterInput.value);
+});
+
+/* CLEAR FILTER INPUT */
+filterClearBtn.addEventListener("click", () => {
+  filterInput.value = "";
+  filterResults(filterInput.value);
+});
+
+/* APPLYING DYNAMIC GENRE TAG CLASS */
+genreTags.forEach((tag) => {
+  tag.classList.add(
+    `tag-genre--${tag.textContent.trim().toLowerCase().replaceAll(" ", "-")}`
+  );
+});
+
+/* CHANGE VIEW MODE */
+const toggleViewMode = function (
+  activeBtnID,
+  inactiveBtnID,
+  showContainerClass,
+  hideContainerClass
+) {
+  document
+    .getElementById(activeBtnID)
+    .classList.add("results-view-btn--active");
+  document
+    .getElementById(inactiveBtnID)
+    .classList.remove("results-view-btn--active");
+
+  document.querySelector(hideContainerClass).classList.add("hide");
+  document.querySelector(showContainerClass).classList.remove("hide");
+};
+
+viewModeBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const isList = btn.id === "list-btn";
+    localStorage.setItem("userViewMode", isList ? "list" : "grid");
+
+    isList
+      ? toggleViewMode(
+          "list-btn",
+          "grid-btn",
+          ".list-view-container",
+          ".grid-view-container"
+        )
+      : toggleViewMode(
+          "grid-btn",
+          "list-btn",
+          ".grid-view-container",
+          ".list-view-container"
+        );
+  });
+});
+
+/* SET VIEW MODE ON PAGE LOAD */
+document.addEventListener("DOMContentLoaded", () => {
+  const savedView = localStorage.getItem("userViewMode");
+
+  if (savedView === "grid") {
+    toggleViewMode(
+      "grid-btn",
+      "list-btn",
+      ".grid-view-container",
+      ".list-view-container"
+    );
+  } else {
+    toggleViewMode(
+      "list-btn",
+      "grid-btn",
+      ".list-view-container",
+      ".grid-view-container"
+    );
+  }
+});
---
+++ routes/browse/albums.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../../db/db");
+const {
+  dbAll,
+  dbGet,
+  convertTimestamp,
+  moreRecentTimestamp,
+} = require("../UTIL.js");
+
+router.get("/", async (req, res) => {
+  const sortKeys = {
+    title: "albums.title",
+    genre: "albums.genre",
+    year: "albums.release_year",
+    artist: "artists.name",
+    added: "albums.timestamp",
+  };
+
+  const sortKey = sortKeys[req.query.sort] || sortKeys.added;
+
+  if (!sortKey) {
+    return res.status(400).send("Invalid sort key");
+  }
+
+  const queries = {
+    totalResults: `
+        SELECT COUNT(*) AS total_results FROM albums`,
+
+    albums: `
+        SELECT
+            albums.id AS album_id,
+            albums.title AS album_title,
+            albums.genre AS album_genre,
+            albums.release_year AS album_release_year,
+            albums.image_url AS album_image,
+            albums.timestamp AS album_added_timestamp,
+            artists.name AS artist_name,
+            artists.id AS artist_id
+        FROM albums
+        LEFT JOIN album_songs ON albums.id = album_songs.album_id
+        LEFT JOIN songs ON album_songs.song_id = songs.id
+        LEFT JOIN song_artists ON songs.id = song_artists.song_id
+        LEFT JOIN artists ON song_artists.artist_id = artists.id
+        WHERE song_artists.role = 'Main' AND albums.title NOT LIKE '[SINGLE]'
+        GROUP BY albums.id
+        ORDER BY ${sortKey}`,
+  };
+
+  try {
+    const [totalResults, albums] = await Promise.all([
+      dbGet(queries.totalResults),
+      dbAll(queries.albums),
+    ]);
+
+    if (albums) {
+      albums.forEach((album) => {
+        album.is_new = moreRecentTimestamp(
+          album.album_added_timestamp,
+          3 * 24 * 60 * 60 * 1000
+        ); // 3 days
+        album.album_added_timestamp = convertTimestamp(
+          album.album_added_timestamp
+        );
+      });
+    }
+
+    res.render("browse/albums", {
+      totalResults: totalResults.total_results,
+      albums,
+    });
+  } catch (err) {
+    return res.status(500).send("Database error: " + err.message);
+  }
+});
+
+module.exports = router;
