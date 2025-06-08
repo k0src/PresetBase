1361352e4d7704792d3f2bf6e096aa26841b2b60
k0 <k0src@null.net>
2025-06-08 17:41:47 -0500

    mobile nav,index queries

--- public/js/partials/navbar.js
+++ public/js/partials/navbar.js
@@ -1,5 +1,6 @@
 const navSearchInput = document.querySelector(".navbar-search--input");
 
+/* --------------------------------- Search --------------------------------- */
 navSearchInput.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
     if (navSearchInput.value) {
@@ -15,4 +16,16 @@ document.addEventListener("keydown", function (event) {
     event.preventDefault();
     navSearchInput.focus();
   }
+});
+
+/* ------------------------------- Mobile Nav ------------------------------- */
+const btnMobileNav = document.querySelector(".button-mobile-nav");
+const mobileNav = document.querySelector(".mobile-nav");
+const iconBars = btnMobileNav.querySelector(".fa-bars");
+const iconX = btnMobileNav.querySelector(".fa-xmark");
+
+btnMobileNav.addEventListener("click", () => {
+  mobileNav.classList.toggle("nav-open");
+  iconBars.classList.toggle("hidden");
+  iconX.classList.toggle("hidden");
 });
