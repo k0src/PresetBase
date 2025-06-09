66d15873c183dcd4b25b3ced8697144ada1655c4
k0 <k0src@null.net>
2025-06-09 01:26:14 -0500

    ignore

--- bs-config.js
+++
@@ -1,9 +0,0 @@
-const browserSync = require("browser-sync").create();
-
-browserSync.init({
-  proxy: "http://localhost:3000",
-  files: ["public/**/*.*", "views/**/*.ejs", "routes/**/*.js"],
-  port: 4000,
-  open: false,
-  notify: false,
-});
