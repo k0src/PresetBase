367bf7e4d98fc4ffb879af1e34ffdc3c1a072d73
k0 <k0src@null.net>
2025-06-13 00:40:48 -0500

    mobile search

--- public/js/partials/navbar.js
+++ public/js/partials/navbar.js
@@ -22,10 +22,21 @@ document.addEventListener("keydown", function (event) {
 const btnMobileNav = document.querySelector(".button-mobile-nav");
 const mobileNav = document.querySelector(".mobile-nav");
 const iconBars = btnMobileNav.querySelector(".fa-bars");
-const iconX = btnMobileNav.querySelector(".fa-xmark");
+const iconClose = btnMobileNav.querySelector(".fa-xmark");
+const mobileSearchInput = document.querySelector(".mobile-nav-search-input");
 
 btnMobileNav.addEventListener("click", () => {
   mobileNav.classList.toggle("nav-open");
   iconBars.classList.toggle("hidden");
-  iconX.classList.toggle("hidden");
+  iconClose.classList.toggle("hidden");
+});
+
+mobileSearchInput.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    if (mobileSearchInput.value) {
+      const searchValue = mobileSearchInput.value.trim().toLowerCase();
+      window.location.href = `/search?query=${encodeURIComponent(searchValue)}`;
+      mobileSearchInput.value = "";
+    }
+  }
 });
