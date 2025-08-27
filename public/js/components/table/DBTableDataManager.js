0c96ee327c95e2f1039d9b65390ecaef587b58b0
k0 <k0src@null.net>
2025-08-27 01:27:58 -0500

    Cleanup

--- public/js/components/table/DBTableDataManager.js
+++
@@ -1,5 +0,0 @@
-export class DBTableDataManager {
-  static async fetchTableData(sortKey = "", sortDirection = "") {
-    throw new Error("fetchTableData must be implemented by subclass");
-  }
-}
--- public/js/partials/tags.js
+++
@@ -1,9 +0,0 @@
-const tags = document.querySelectorAll(".--tag, .--tag--small");
-
-if (tags.length !== 0) {
-  tags.forEach((tag) => {
-    tag.classList.add(
-      `tag--${tag.textContent.trim().toLowerCase().replaceAll(" ", "-")}`
-    );
-  });
-}
